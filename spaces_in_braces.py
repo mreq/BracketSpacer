21384b7f7d8bebef7e62382a793d4c64bc284dc1
Petr Marek <contact@petrmarek.eu>
2017-04-19 22:14:50 +0200

    remove_spaces_in_braces

--- spaces_in_braces.py
+++ spaces_in_braces.py
@@ -30,6 +30,7 @@ def find_next_bracket(view, line, bracket, start):
 
     return found
 
+
 class AddSpacesInBraces(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
@@ -53,3 +54,23 @@ class AddSpacesInBraces(sublime_plugin.TextCommand):
             if (sel.a == matching_bracket_pos):
                 view.sel().clear()
                 view.sel().add(sublime.Region(sel.a + 1, sel.a + 1))
+
+
+class RemoveSpacesInBraces(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        sel = view.sel()[0]
+        line = view.line(sel)
+        line_text = view.substr(line)
+        bracket = view.substr(sublime.Region(sel.a - 2, sel.a - 1))
+
+        view.run_command('left_delete')
+
+        if bracket not in opposite_brackets:
+            return
+
+        matching_bracket_pos = find_next_bracket(view, line, bracket, sel.b)
+
+        if matching_bracket_pos:
+            view.erase(edit, sublime.Region(matching_bracket_pos - 1,
+                                            matching_bracket_pos))
